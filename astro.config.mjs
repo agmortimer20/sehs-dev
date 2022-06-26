b36e984c6d62698fd344fbfa32eecbfe5edb2403
Anthony Mortimer <agmortimer20@gmail.com>
2022-06-25 21:02:04 -0400

    Update config to use defineConfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
+import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 
-export default {
+export default defineConfig({
   integrations: [svelte()],
   markdown: {
     syntaxHighlight: 'prism'
   }
-};
+});
